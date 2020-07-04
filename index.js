465f432822ba20526e0799bc1b8768b4049c9a6e
Austin <l02162010@gmail.com>
2020-07-04 15:26:06 +0800

    cancel secure

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const SMTPServer = require('smtp-server').SMTPServer;
 
 const mailServer = new SMTPServer({
   authOptional: true,
+  secure: false,
+  disabledCommands: ['AUTH', 'STARTTLS'],
   onData(stream, session, callback) {
     stream.pipe(process.stdout);
     stream.on('end', callback);
