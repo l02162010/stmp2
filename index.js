0113e1373f64670eda657113b745169bba43df39
Austin <l02162010@gmail.com>
2020-07-04 16:03:48 +0800

    add error log

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const SMTPServer = require('smtp-server').SMTPServer;
 
 const mailServer = new SMTPServer({
-  authOptional: true,
+  banner: 'hi',
   secure: false,
   disabledCommands: ['AUTH', 'STARTTLS'],
   onData(stream, session, callback) {
+    console.log(session)
+    console.log(stream)
     stream.pipe(process.stdout);
     stream.on('end', callback);
   }
 });
-
+mailServer.on("error", err => {
+  console.log("Error %s", err.message);
+});
 mailServer.listen(25, () => {
   console.log('Listening for mail..');
 });
