644c63b1a46285b5555c1ebb25145b73833581b9
Austin <l02162010@gmail.com>
2020-07-04 15:16:03 +0800

    disable auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const SMTPServer = require('smtp-server').SMTPServer;
 
 const mailServer = new SMTPServer({
+  authOptional: true,
   onData(stream, session, callback) {
     stream.pipe(process.stdout);
     stream.on('end', callback);
